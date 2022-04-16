561fd48c112be6017a06a69baae3e6f4ac738b05
takasu-atsuki <89208619+takasu-atsuki@users.noreply.github.com>
2022-04-16 23:07:29 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,54 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+class Todo {
+  constructor() {
+    this.todos = [
+      {
+        id: 1,
+        task: "勉強",
+        finishe: false
+      },
+      {
+        id: 2,
+        task: "片付け",
+        finishe: false
+      },
+      {
+        id: 3,
+        task: "読書",
+        finishe: true
+      }
+    ];
+    this.createbutton = document.getElementById("todo_create_button");
+    this.update_tag_list = document.getElementById("todo_update_tag");
+    this.complete_task_list = document.getElementById("complete-task-list");
+    this._init();
+    this._addEvent();
+  }
+  _init() {
+    this.todos.forEach((object) => {
+      if (!object.finishe) {
+        const update_tag = `<li class="list"><div class="list-row"><p>${object.task}</p><button>完了</button><button>削除</button></div></li>`;
+        this.update_tag_list.innerHTML += update_tag;
+      } else {
+        const update_tag = `<li class="list"><div class="list-row"><p>${object.task}</p><button>戻す</button></div></list>`;
+        this.complete_task_list.innerHTML += update_tag;
+      }
+    });
+  }
+  _addEvent() {
+    this.createbutton.addEventListener("click", this.createTodo.bind(this));
+  }
+  createTodo() {
+    const create_todo = document.getElementById("todo").value;
+    this.todos.push({
+      id: this.todos.length - 1,
+      task: create_todo,
+      finishe: false
+    });
+    console.log(this.todos);
+    this._init();
+  }
+}
+
+new Todo();
